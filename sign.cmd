31a4e1d7652cd48e2c6fa2c0aa5e43d57952a678
Ruediger Jungbeck <ruediger.jungbeck@rsj.de>
2025-07-03 17:00:28 +0200

    Added sign.yml for test

---
+++ sign.cmd
@@ -0,0 +1,5 @@
+for /f "usebackq tokens=* delims=" %%i in (`gcloud auth print-access-token`) do (
+    set "GCP_ACCESS_TOKEN=%%i"
+)
+
+java -jar jsign-7.1.jar  --storetype GOOGLECLOUD --keystore projects/key-mangment-464714/locations/global/keyRings/codesigning --storepass %GCP_ACCESS_TOKEN% --alias cs2022-3 --certfile cs2022cert.pem -t http://timestamp.digicert.com notepad.exe
